89e7bb2094f572fe6e3cdf9fe365f2e672bcea58
Caitlin <caitlinramsey@outlook.com>
2023-11-22 15:03:22 -0500

    Got landing page and routing to homepage working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import Landing from '../src/pages/landing/Landing'
-import Home from '../src/pages/home/Home'
-import './App.css';
+import Landing from "../src/pages/landing/Landing";
+import Home from "../src/pages/home/Home";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Landing />
-      <Home />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,24 +1,27 @@
 import logo from "../../logo.svg";
 import "./landing.css";
+import Home from '../home/Home'
 
 function Landing() {
-  <div className="landing">
-    <header className="landing-header">
-      <img src={logo} className="landing-logo" alt="logo" />
-      <p>
-        Hi! I'm Caitlin. A Full-Stack Developer, Graphic Designer, and
-        Photographer.
-      </p>
-      <a
-        className="Home-link"
-        href="/Home"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        See My Work
-      </a>
-    </header>
-  </div>;
+  return (
+    <div className="landing">
+      <header className="landing-header">
+        <img src={logo} className="landing-logo" alt="logo" />
+        <p>
+          Hi! I'm Caitlin. A Full-Stack Developer, Graphic Designer, and
+          Photographer.
+        </p>
+        <a
+          className="home-link"
+          href="Home"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See My Work
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default Landing;
