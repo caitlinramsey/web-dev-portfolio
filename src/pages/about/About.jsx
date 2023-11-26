6bf7ec5d5353f24f9049c2d5fe254abacf819638
Caitlin <caitlinramsey@outlook.com>
2023-11-25 22:15:09 -0500

    Added font styles and worked on styling about me page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -6,7 +6,7 @@ import { Slide } from 'react-awesome-reveal';
 function About() {
     return (
         <Slide direction='right' triggerOnce>
-        <section id='about-me' className='about-me pt-5'>
+        <section id='about-me' className='about-me'>
             <h1 className='text-center'>About Me</h1>
 
             <Container className='about-me-container'>
@@ -14,7 +14,7 @@ function About() {
                     <div className='about-me-image'></div>
                 </div>
 
-                <div className='about-me-content text-center'>
+                <div className='about-me-content text-center mb-5'>
                     <p>
                         I graduated from Cedar Crest College in May 2019 with a Bachelor of Science in Environmental Conservation and a Bachelor of Arts in New Media. In August of 2023, I graduated from a 6-month course through the University of North Carolina at Chapel Hill with a certificate in Full-Stack Development. I am one class away from completing a graduate certificate in Fisheries Management through Oregon State University.
                         <br/><br/>
