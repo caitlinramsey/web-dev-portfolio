2efb79fd763ae64c8f20afbb430e8a9b435787aa
Caitlin <caitlinramsey@outlook.com>
2024-01-09 15:16:29 -0500

    Added link to prototype to figma image as well as text

--- src/pages/graphic-design-projects/GraphicDesignProjects.jsx
+++ src/pages/graphic-design-projects/GraphicDesignProjects.jsx
@@ -35,7 +35,10 @@ function GraphicDesignProjects() {
         <Slide direction="left" triggerOnce>
           <div className="figma">
             <h1 className="figma-title text-center bg-transparent">Figma</h1>
-            <img src={tiktokFigma} className='tiktok-figma' alt='a design I made in Figma that is a recreation of tiktok' />
+            <h3 className="prototype text-center text-white">Click on the image to be taken to the prototype!</h3>
+            <a href="https://www.figma.com/proto/yaGrdqtrMlKF5XaEIIhARp/First-Design-Challenge---TikTok-MockUp?type=design&node-id=3-2&t=DdWEUzB4UmafCJc9-0&scaling=scale-down&page-id=0%3A1&starting-point-node-id=3%3A2" target="_blank" rel="noopener noreferrer">
+              <img src={tiktokFigma} className='tiktok-figma' alt='a design I made in Figma that is a recreation of tiktok' />
+            </a>
           </div>
         </Slide>
 
