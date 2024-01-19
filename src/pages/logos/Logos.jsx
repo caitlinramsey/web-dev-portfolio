77496df4467e54be3cc8224de76e79442b3a7875
Caitlin <caitlinramsey@outlook.com>
2024-01-18 19:35:42 -0500

    Added new logos to logo page

--- src/pages/logos/Logos.jsx
+++ src/pages/logos/Logos.jsx
@@ -1,6 +1,8 @@
 import './logos.css';
 import ddcLogo from '../../assets/graphic-design/logos/ddc-logo.jpg';
 import rocketLogo from '../../assets/graphic-design/logos/Day-1-rocket.png';
+import balloonLogo from '../../assets/graphic-design/logos/Day-2-balloon.png';
+import pandaLogo from '../../assets/graphic-design/logos/Day-3-panda.png';
 import { Slide } from "react-awesome-reveal";
 import Container from "react-bootstrap/Container";
 
@@ -12,8 +14,10 @@ function Logos() {
           <Slide direction="left" triggerOnce>
             <div className="logos">
               <div className="logo-projects">
+                <img src={pandaLogo} className='rocketlogo' alt="a logo from an exercise I did" />
                 <img src={ddcLogo} className='ddclogo' alt="the logo for my company" />
                 <img src={rocketLogo} className='rocketlogo' alt="a logo from an exercise I did" />
+                <img src={balloonLogo} className='rocketlogo' alt="a logo from an exercise I did" />
               </div>
             </div>
           </Slide>
