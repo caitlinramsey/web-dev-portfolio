361d37550804744c9df4f13c92e4140062632ee0
Caitlin <caitlinramsey@outlook.com>
2024-01-22 17:21:44 -0500

    Added responsivity to logo page and new logo

--- src/pages/logos/Logos.jsx
+++ src/pages/logos/Logos.jsx
@@ -1,10 +1,11 @@
 import './logos.css';
+import { Slide } from "react-awesome-reveal";
+import Container from "react-bootstrap/Container";
 import ddcLogo from '../../assets/graphic-design/logos/ddc-logo.jpg';
 import rocketLogo from '../../assets/graphic-design/logos/Day-1-rocket.png';
 import balloonLogo from '../../assets/graphic-design/logos/Day-2-balloon.png';
 import pandaLogo from '../../assets/graphic-design/logos/Day-3-panda.png';
-import { Slide } from "react-awesome-reveal";
-import Container from "react-bootstrap/Container";
+import inItsPlace from '../../assets/graphic-design/logos/in-its-place.png';
 
 function Logos() {
     return (
@@ -14,10 +15,11 @@ function Logos() {
           <Slide direction="left" triggerOnce>
             <div className="logos">
               <div className="logo-projects">
-                <img src={pandaLogo} className='rocketlogo' alt="a logo from an exercise I did" />
+                <img src={pandaLogo} className='rocketlogo' alt="a panda logo from an exercise I did" />
+                <img src={inItsPlace} className='initsplace' alt="a logo for a cleaning company" />
                 <img src={ddcLogo} className='ddclogo' alt="the logo for my company" />
-                <img src={rocketLogo} className='rocketlogo' alt="a logo from an exercise I did" />
-                <img src={balloonLogo} className='rocketlogo' alt="a logo from an exercise I did" />
+                <img src={rocketLogo} className='rocketlogo' alt="a rocket logo from an exercise I did" />
+                <img src={balloonLogo} className='rocketlogo' alt="a hot air balloon logo from an exercise I did" />
               </div>
             </div>
           </Slide>
