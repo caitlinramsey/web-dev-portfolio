14f95f13c8764da2d429c504b1751126110aad52
Caitlin <caitlinramsey@outlook.com>
2024-01-22 21:45:12 -0500

    Added new logo and improved naming

--- src/pages/logos/Logos.jsx
+++ src/pages/logos/Logos.jsx
@@ -2,10 +2,11 @@ import './logos.css';
 import { Slide } from "react-awesome-reveal";
 import Container from "react-bootstrap/Container";
 import ddcLogo from '../../assets/graphic-design/logos/ddc-logo.jpg';
-import rocketLogo from '../../assets/graphic-design/logos/Day-1-rocket.png';
-import balloonLogo from '../../assets/graphic-design/logos/Day-2-balloon.png';
-import pandaLogo from '../../assets/graphic-design/logos/Day-3-panda.png';
+import rocketLogo from '../../assets/graphic-design/logos/rocket.png';
+import balloonLogo from '../../assets/graphic-design/logos/balloon.png';
+import pandaLogo from '../../assets/graphic-design/logos/panda.png';
 import inItsPlace from '../../assets/graphic-design/logos/in-its-place.png';
+import pugIcon from '../../assets/graphic-design/logos/pug-icon.png';
 
 function Logos() {
     return (
@@ -17,9 +18,10 @@ function Logos() {
               <div className="logo-projects">
                 <img src={pandaLogo} className='rocketlogo' alt="a panda logo from an exercise I did" />
                 <img src={inItsPlace} className='initsplace' alt="a logo for a cleaning company" />
+                <img src={pugIcon} className='pug-icon' alt='a design I made in Canva that is an app icon' />
                 <img src={ddcLogo} className='ddclogo' alt="the logo for my company" />
                 <img src={rocketLogo} className='rocketlogo' alt="a rocket logo from an exercise I did" />
-                <img src={balloonLogo} className='rocketlogo' alt="a hot air balloon logo from an exercise I did" />
+                <img src={balloonLogo} className='balloonlogo' alt="a hot air balloon logo from an exercise I did" />
               </div>
             </div>
           </Slide>
