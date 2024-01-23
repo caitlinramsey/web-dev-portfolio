e182ec295d102b0907c19ef8c60383dcf028201b
Caitlin <caitlinramsey@outlook.com>
2024-01-22 22:17:49 -0500

    Added a new logo

--- src/pages/logos/Logos.jsx
+++ src/pages/logos/Logos.jsx
@@ -9,6 +9,7 @@ import inItsPlace from '../../assets/graphic-design/logos/in-its-place.png';
 import pugIcon from '../../assets/graphic-design/logos/pug-icon.png';
 import theRoastedBean from '../../assets/graphic-design/logos/the-roasted-bean.png';
 import svenTheFjord from '../../assets/graphic-design/logos/sven-the-fjord.png';
+import nordicFjordPhoto from '../../assets/graphic-design/logos/nordic-fjord-photography.png';
 
 function Logos() {
     return (
@@ -20,6 +21,7 @@ function Logos() {
               <div className="logo-projects">
                 <img src={pandaLogo} className='rocketlogo' alt="a panda logo from an exercise I did" />
                 <img src={inItsPlace} className='initsplace' alt="a logo for a cleaning company" />
+                <img src={nordicFjordPhoto} className='nordic-fjord-photo' alt="a logo for my photography business" />
                 <img src={pugIcon} className='pug-icon' alt='a design I made in Canva that is an app icon' />
                 <img src={theRoastedBean} className='the-roasted-bean' alt='a logo for a coffee shop' />
                 <img src={svenTheFjord} className='sven-the-fjord' alt="a logo for for a horse brand" />
