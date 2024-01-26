8115625352950bdc877cf0a5b03e09861650e010
Caitlin <caitlinramsey@outlook.com>
2024-01-25 21:29:08 -0500

    Added new logos to logo page

--- src/pages/logos/Logos.jsx
+++ src/pages/logos/Logos.jsx
@@ -11,6 +11,9 @@ import theRoastedBean from '../../assets/graphic-design/logos/the-roasted-bean.p
 import svenTheFjord from '../../assets/graphic-design/logos/sven-the-fjord.png';
 import nordicFjordPhoto from '../../assets/graphic-design/logos/nordic-fjord-photography.png';
 import onward from '../../assets/graphic-design/logos/onward.png';
+import snowdrop from '../../assets/graphic-design/logos/snowdrop.png';
+import pitch from '../../assets/graphic-design/logos/pitch.png';
+import flintAndFlame from '../../assets/graphic-design/logos/flint-and-flame.png';
 
 function Logos() {
     return (
@@ -26,8 +29,11 @@ function Logos() {
                 <img src={pugIcon} className='pug-icon' alt='a design I made in Canva that is an app icon' />
                 <img src={theRoastedBean} className='the-roasted-bean' alt='a logo for a coffee shop' />
                 <img src={svenTheFjord} className='sven-the-fjord' alt="a logo for for a horse brand" />
+                <img src={snowdrop} className='snowdrop' alt="a logo for for a skiing brand" />
                 <img src={ddcLogo} className='ddclogo' alt="the logo for my company" />
                 <img src={onward} className='onward' alt="the logo for a driverless car company" />
+                <img src={pitch} className='pitch' alt="the logo for a music company" />
+                <img src={flintAndFlame} className='flint-and-flame' alt="the logo for a firewood company" />
                 <img src={rocketLogo} className='rocketlogo' alt="a rocket logo from an exercise I did" />
                 <img src={balloonLogo} className='balloonlogo' alt="a hot air balloon logo from an exercise I did" />
               </div>
